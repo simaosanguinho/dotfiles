e7d2ba301b4bb7bafe9472e52022c325a10ca9b0
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-09-13 09:44:17 +0100

    feat: add core modules

---
+++ modules/core/git.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Simão Sanguinho";
+    userEmail = "simao.sanguinho@tecnico.ulisboa.pt";
+    extraConfig = {
+      color.ui = true;
+      pull.rebase = true;
+      init.defaultBranch = "main";
+      core.editor = "nano";
+    };
+    diff-so-fancy = { enable = true; };
+  };
+}
