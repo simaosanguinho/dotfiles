ab7c4cfb6b82a1988139d0f064206c65158ee0b5
Simão Sanguinho <simao.sanguinho@rnl.tecnico.ulisboa.pt>
2025-02-04 09:03:07 +0000

    fix: remove authorized keys files from ssh config

--- modules/core/sshd.nix
+++ modules/core/sshd.nix
@@ -1,7 +1,6 @@
 { lib, sshKeys, username, ... }: {
   services.openssh = {
     enable = true;
-    authorizedKeysFiles = [ "/var/lib/openssh-server/%u" "/home/%u/.ssh/authorized_keys" ];
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
